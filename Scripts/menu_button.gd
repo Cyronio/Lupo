817e1a06f997ee372985a1d263b6e9ab2edf9472
CM <cyro12@gmx.de>
2025-07-31 20:44:22 +0200

    Title added

---
+++ Scripts/menu_button.gd
@@ -0,0 +1,15 @@
+extends MenuButton
+
+@onready var game_manager: Node = get_node("/root/Game/GameManager")
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_pressed() -> void:
+	game_manager.level_cleared()
